27995f16ad10d36272ddb0ad3229678c3e25f285
christianraymond <ombenisn.raymond@gmail.com>
2017-08-11 16:29:11 +0200

    greeting exercise continue

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
-
 const app = express();
 // app.disable('x-powered-by');
-const handlebars = require('express-handlebars').create({defaultLayout:'main'});
+const handlebars = require('express-handlebars').create({
+  defaultLayout: 'main'
+});
 const bodyParser = require('body-parser');
-// const greetingRoutes = require('greeted');
+// const greetedNameRoutes = require('greetedName');
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
@@ -12,25 +14,53 @@ app.use(express.static('public'));
 
 // parse application/x-www-form-urlencoded
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 //MORE IMPORTS HERE
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
-    res.render('home');
+app.get('/', function(req, res) {
+  res.render('home');
 });
 
-app.get('/about', function(req, res){
-    res.render('about');
+app.get('/about', function(req, res) {
+  res.render('about');
 });
 
-app.get('/contact', function(req, res){
-    res.render('contact');
+app.get('/contact', function(req, res) {
+  res.render('contact');
 });
+
+app.get('/greetedName', function(req, res){
+  res.render('greetedName')
+})
 // app.post('/greeted/name', greetingRoutes.name);
 
-app.listen(app.get('port'), function(){
-    console.log("App runnning on http://localhost:" + app.get('port'));
+app.listen(app.get('port'), function() {
+  console.log("App runnning on http://localhost:" + app.get('port'));
+});
+
+app.post('/greeted', function(req, res) {
+  var name = req.body.name;
+  var language = req.body.language;
+  console.log(name);
+
+  if (language === 'isixhosa') {
+    var greetedName = 'Molo ' + name;
+  }
+  if (language === 'french') {
+    var greetedName = 'Bonjour ' + name;
+  };
+  if (language === 'english') {
+    var greetedName = 'Hello ' + name;
+  };
+
+
+var data = {
+  greetedName: greetedName,
+}
+res.render('home', data);
 });
--- public/greeting.js
+++ public/greeting.js
@@ -1,55 +1,5 @@
-module.exports = function(models){
+module.exports = function(greetedName){
 
-    const nameList = [];
-
-    const index = function(req, res, next){
-
-        models.greeted.find({}, function(err, greeted){
-            if (err){
-                return next(err);
-            }
-            res.render('subjects/index', {subjects});
-        });
-
-    };
-
-    const addScreen = function(req, res){
-        res.render('subjects/add');
-    }
-
-    const add = function(req, res, next){
-        //res.send('Add a subject');
-
-        var subject = {
-            name : req.body.subject
-        };
-
-        if (!subject || !subject.name){
-            req.flash('error', 'Subject should not be blank');
-            res.redirect('/subjects');
-        }
-        else{
-            models.Subject.create(subject, function(err, results){
-                if (err){
-                    if (err.code === 11000){
-                        req.flash('error', 'Subject already exists!');
-                    }
-                    else{
-                        return next(err);
-                    }
-                }
-                else{
-                    req.flash('success', 'Subject added!');
-                }
-                res.redirect('/subjects');
-            });
-        }
-    }
-
-    return {
-        index,
-        add,
-        addScreen
-    }
+    const NameList = [];
 
 }
